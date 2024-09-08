35c60d2c1de690c77eae8f85960dba306c69b29a
Annytech001 <godlybassey101>
2024-09-08 23:59:57 +0100

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,13 @@
-import Navbar from  './components/Navbar';
+// import Navbar from  './components/Navbar';
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="stickly top-0 z-50 py-3 backdrop-blur-lg border-b
+    border-neutral-700/80">
+    <div className="container mx-auto px-4 relative text-sm"/>
+     <div className="flex justify-center items-center"/>
+     <div className="flex items-center flex-shrink-0"/>
+    </nav>
   )
 }
 
